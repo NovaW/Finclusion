a332adc5a9c50d5093a61776009cb1a596f986cb
nova <nova.wiffen@genexinstitute.com>
2022-02-20 15:10:25 +0200

    updated account model in db

---
+++ Finclusion.Database/Migrations/20220220130920_AccountBalance.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finclusion.Database.Migrations
+{
+    public partial class AccountBalance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Balance",
+                table: "Accounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Finclusion.Database/Models/Account.cs
+++ Finclusion.Database/Models/Account.cs
@@ -6,5 +6,5 @@ public class Account
 {
     public int Id { get; set; }
     public string Username { get; set; }
-    public string Password { get; set; }
+    public int Balance { get; set; }
 }
